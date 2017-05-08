4a9b1b04f91776e004cb35fb4508bd9be2980aa1
Joshua Clayton <joshua.clayton@gmail.com>
2017-05-08 14:55:40 -0400

    Specify path to elm-make
    Why?
    ====
    
    When elm-make is not available in $PATH, we need to reference an
    explicit path when compiling assets, e.g. on Heroku.
    
    This explicitly sets the path to elm-make, since it will be available in
    the node_modules directory.

--- config/webpack/loaders/elm.js
+++ config/webpack/loaders/elm.js
@@ -5,5 +5,5 @@ const elmSource = path.resolve(process.cwd())
 module.exports = {
   test: /\.elm$/,
   exclude: [/elm-stuff/, /node_modules/],
-  loader: `elm-hot-loader!elm-webpack-loader?verbose=true&warn=true&debug=true&cwd=${elmSource}`
+  loader: `elm-hot-loader!elm-webpack-loader?pathToMake=node_modules/.bin/elm-make&verbose=true&warn=true&debug=true&cwd=${elmSource}`
 }
